1773fa8b2306c7ad3f2aea1ae955342f9ff66fa9
seghemwanre123 <86158146+seghemwanre@users.noreply.github.com>
2021-07-24 12:08:03 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,64 @@
+'''
+For in Loop
+'''
+#students = ['Bob', 'Adriana', 'Felix']
+
+#for x in students:
+ # print ('hi',x)
+
+#====================
+
+
+# print (range(10))
+# print (list(range(10)))
+# print (list(range(3,6)))
+# print (list(range(3,12,3)))
+
+#
+#for x in range (1,6,2):
+ # print (x+1)
+
+#for i in range (1, 6):
+  #for j in range (i):
+  #  print ("*", end= " ")
+ # print()
+
+#====================
+'''
+While Loop
+'''
+#i = 1 
+#while i < 10: 
+ # print (i)
+  #i += 1 
+
+#====================
+# break statement
+#i = 1 
+#while i < 10: 
+ # print (i)
+  #if i == 3:
+  #  break
+  #i += 1 
+
+#====================
+# continue statement
+#i = 1 
+#while i < 5: 
+  #i += 1 
+  #if i == 4:
+  #  continue
+  #print (i)
+
+#====================
+#for i in range (1 , 10):
+    #if i == 3:
+    # break 
+    #print (i)
+
+
+
+   
+
+
+  
